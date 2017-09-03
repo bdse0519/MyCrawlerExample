d5f3cefe52afc30cf15b65a094368de3ae406f4a
bdse0519 <shunn2322@yahoo.com.tw>
2017-09-03 16:26:27 +0800

    fbElastic修改

--- src/crawler/example/integration/FBElasticImport.java
+++ src/crawler/example/integration/FBElasticImport.java
@@ -14,7 +14,7 @@ public class FBElasticImport {
     static String elasticPort = "9200" ;
     static String elasticIndex = "bdse0519";
     static String elasticIndexType = "data";
-    static String pageName = "starbuckstaiwan";
+    static String pageName = "starbuckstaiwan";//尋找starbuckstaiwan
     // 2017-09-02
     static long start = 1504363907;
     // 往前抓抓取日期數
